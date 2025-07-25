f83ddf4b07c3712236dfb6c8820995c5c6ef9524
Your Name <you@example.com>
2025-07-25 12:12:12 +0000

    build: add timm

--- programs/test_whisper.py
+++ programs/test_whisper.py
@@ -8,10 +8,10 @@ source: https://github.com/openai/whisper
 import whisper
 
 model = whisper.load_model("turbo")
-result = model.transcribe("whisper_inputs/Saad_01.mp3")
+result = model.transcribe("whisper_inputs/Judaism_04.mp3")
 
 # Specify the output file name
-output_file = "whisper_outputs/transcription.txt"
+output_file = "whisper_outputs/transcription_Judaism_04.txt"
 
 # Write the transcription result to the file
 with open(output_file, "w", encoding="utf-8") as f:
