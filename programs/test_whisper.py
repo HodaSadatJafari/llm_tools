3def93876eb18d5a3a2f9c6ff8df58d7e0ba33b0
Hoda Sadat Jafari <hodas.jafari@gmail.com>
2025-07-25 10:28:31 +0330

    feat: save the transcript into file

--- programs/test_whisper.py
+++ programs/test_whisper.py
@@ -1,9 +1,20 @@
 """
 !pip install -U openai-whisper
+source: https://github.com/openai/whisper
 """
 
 import whisper
 
-model = whisper.load_model("medium")
-result = model.transcribe("/home/hoda/Desktop/llms_test/voices/farsi_woman_voice.mp3")
-print(result["text"])
+model = whisper.load_model("turbo")
+result = model.transcribe("https://khorshid.info/q/Qlist/Surahs/Saad/Saad_01.mp3")
+
+# Specify the output file name
+output_file = "whisper_outputs/transcription.txt"
+
+# Write the transcription result to the file
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(result["text"])
+
+print(f"Transcription saved to {output_file}")
+
+# print(result["text"])
