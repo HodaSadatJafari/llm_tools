455cdbd7608b4e5ec0c727989aa790a7e237f65a
Hoda Sadat Jafari <hodas.jafari@gmail.com>
2025-07-27 09:47:16 +0330

    fix: test_chat for qwen with thinking

--- programs/test_chat.py
+++ programs/test_chat.py
@@ -17,40 +17,63 @@ def generate_response(user_input, history):
         messages,
         tokenize=False,
         add_generation_prompt=True,
+        enable_thinking=True,  # Switches between thinking and non-thinking modes. Default is True.
     )
 
     print(f"Model device {model.device}")  # test .to('cuda')
     inputs = tokenizer(
-        text,
+        [text],
         return_tensors="pt",
     ).to(model.device)
 
-    # Generate response
-    response = ""
+    # conduct text completion
+    generated_ids = model.generate(**inputs, max_new_tokens=32768)
+    output_ids = generated_ids[0][len(inputs.input_ids[0]) :].tolist()
+
+    # parsing thinking content
     try:
-        response_ids = model.generate(
-            **inputs,
-            max_new_tokens=512,
-        )
-        generated_text = tokenizer.decode(
-            response_ids[0][inputs.input_ids.shape[1] :],
-            skip_special_tokens=True,
-        )
+        # rindex finding 151668 (</think>)
+        index = len(output_ids) - output_ids[::-1].index(151668)
+    except ValueError:
+        index = 0
 
-        response = generated_text.strip()
+    thinking_content = tokenizer.decode(
+        output_ids[:index], skip_special_tokens=True
+    ).strip("\n")
+    content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
 
-        print(f"***** Response: {response}")
-        # Yield the response incrementally
-        # for i in range(1, len(response) + 1):
-        #     yield response[:i]
-        for i in range(len(response)):
-            yield response[: i + 1]
+    print("***thinking content:", thinking_content)
+    # print("content:", content)
 
-    except Exception as e:
-        yield f"An error occurred: {str(e)}"
+    for i in range(len(content)):
+        yield content[: i + 1]
+
+    # Generate response
+    # response = ""
+    # try:
+    #     response_ids = model.generate(
+    #         **inputs,
+    #         max_new_tokens=32768,
+    #     )
+    #     generated_text = tokenizer.decode(
+    #         response_ids[0][inputs.input_ids.shape[1] :],
+    #         skip_special_tokens=True,
+    #     )
+
+    #     response = generated_text.strip()
+
+    #     print(f"***** Response: {response}")
+    #     # Yield the response incrementally
+    #     # for i in range(1, len(response) + 1):
+    #     #     yield response[:i]
+    #     for i in range(len(response)):
+    #         yield response[: i + 1]
+
+    # except Exception as e:
+    #     yield f"An error occurred: {str(e)}"
 
 
-model_name = "Qwen/Qwen3-1.7B"
+model_name = "Qwen/Qwen3-4B"
 # "Qwen/Qwen3-4B"
 # "Qwen/Qwen3-1.7B"
 # "Qwen/Qwen3-32B"
@@ -59,9 +82,7 @@ model_name = "Qwen/Qwen3-1.7B"
 print(f"Input model: {model_name}")
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype=torch.float16,
-    device_map="auto",
+    model_name, torch_dtype="auto", device_map="auto"
 )
 
 with gr.Blocks() as demo:
